e31bc86dadc3d8ad773e5914cf6f11d132b9785c
유주은 <jeyoo7@naver.com>
2022-07-31 15:29:56 +0900

    음악 추가하기

--- pickever/views.py
+++ pickever/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Music
+from .forms import MusicForm
 
 def index(request):
-    return HttpResponse("안녕하세요 피케버 입니다.")
+    music_list = Music.objects.order_by('-create_date')
+    context = {'music_list': music_list}
+    return render(request, 'pickever/music_list.html', context)
+
+def music_create(request):
+    if request.method == 'POST':
+        form = MusicForm(request.POST)
+        if form.is_valid():
+            music = form.save(commit=False)
+            music.create_date = timezone.now()
+            music.save()
+            return redirect('pickever:index')
+    else:
+        form = MusicForm()
+    context = {'form': form}
+    return render(request, 'pickever/music_form.html', context)
