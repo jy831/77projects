5be5da8a2ed226647d08155e240d3edffb17ff80
LAPTOP-RMHFH64I\72kdr <72kdr@naver.com>
2022-08-02 20:33:42 +0900

    08.02 삭제버튼&로그인필수 능력

--- pickever/urls.py
+++ pickever/urls.py
@@ -6,4 +6,5 @@ app_name = 'pickever'
 urlpatterns = [
     path('', views.index, name='index'),
     path('music/create/', views.music_create, name='music_create'),
+    path('music/delete/<int:music_id>/', views.music_delete, name='music_delete'),
 ]
