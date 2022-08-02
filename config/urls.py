3c377de0ab3feb28cc425175a2d10a2d8152ea3b
유주은 <jeyoo7@naver.com>
2022-08-02 14:56:09 +0900

    회원가입

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pickever import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pickever/', include('pickever.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),
 ]
