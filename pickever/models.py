950d6a5d65739c5bed341e36a3b2e46378ede4f8
DESKTOP-RPN8EOR\김재윤 <jaeyun831025@naver.com>
2022-08-02 17:12:42 +0900

    모델 변경

--- pickever/models.py
+++ pickever/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Music(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
     create_date = models.DateTimeField()
+    voter=models.ManyToManyField(User, related_name='voter')
+
+
 
     def __str__(self):
         return f'{self.title} - {self.artist}'
