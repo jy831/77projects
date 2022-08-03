ad5210045cc7ae39a7b00ca02a40246fd6c8e215
DESKTOP-RPN8EOR\김재윤 <jaeyun831025@naver.com>
2022-08-03 15:07:37 +0900

    ㅋㅋ

--- pickever/urls.py
+++ pickever/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('music/create/', views.music_create, name='music_create'),
     path('music/delete/<int:music_id>/', views.music_delete, name='music_delete'),
 path('music/vote/<int:music_id>/', views.music_vote, name='music_vote'),
-    path('report/create/', views.report_create, name='report_create'),
+    path('report/create/<int:music_id>/', views.report_create, name='report_create'),
 ]
